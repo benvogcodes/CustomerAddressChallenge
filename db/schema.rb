4bec8f0ffe2261b925e30a529c10681143ceb045
benvogcodes <benjamin.voglewede@gmail.com>
2016-06-30 12:30:27 -0700

    Move test dir contents to spec dir, Add Simplecov call to spec_helper.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_160_630_000_318) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 20160630000318) do
 
-  create_table 'customer_shipping_addresses', force: :cascade do |t|
-    t.string   'first_name', null: false
-    t.string   'last_name',  null: false
-    t.string   'address',    null: false
-    t.string   'city',       null: false
-    t.string   'state',      null: false
-    t.string   'zip_code',   null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "customer_shipping_addresses", force: :cascade do |t|
+    t.string   "first_name", null: false
+    t.string   "last_name",  null: false
+    t.string   "address",    null: false
+    t.string   "city",       null: false
+    t.string   "state",      null: false
+    t.string   "zip_code",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
