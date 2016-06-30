43737078e85a6e224db05dfc3ca0b201e0f4a189
benvogcodes <benjamin.voglewede@gmail.com>
2016-06-30 16:35:05 -0700

    Finish controller tests, 100% test coverage score with Simplecov

--- spec/controllers/customer_shipping_addresses_controller_spec.rb
+++ spec/controllers/customer_shipping_addresses_controller_spec.rb
@@ -7,32 +7,47 @@ RSpec.describe CustomerShippingAddressesController, type: :controller do
     end
 
     it 'renders the index view' do
-
+      get :index
+      expect(response).to render_template :index
     end
   end
 
   describe 'POST #import' do
+    let(:bad_uploaded){ ActionDispatch::Http::UploadedFile.new({
+    :tempfile => File.new(Rails.root.join('spec/support/customer_shipping_addresses_BAD.csv'))
+    }) }
+    let(:good_uploaded){ ActionDispatch::Http::UploadedFile.new({
+    :tempfile => File.new(Rails.root.join('spec/support/customer_shipping_addresses.csv'))
+    })}
+
     context "with params['addresses']" do
       context 'import validation succeeds' do
-        it 'rerenders the index view with a notice that addresses were imported' do
-
+        it 'redirects to index with a notice that addresses were imported' do
+          post :import, addresses: good_uploaded
+          expect(response).to redirect_to root_url
+          expect(flash[:notice]).to eq('Addresses imported')
         end
       end
 
       context 'import validation fails' do
-        it 'rerenders the index view with a notice that import failed validation' do
-
+        it 'redirects to index with a notice that import failed validation' do
+          post :import, addresses: bad_uploaded
+          expect(response).to redirect_to root_url
+          expect(flash[:notice]).to eq('Import failed validation')
         end
 
-        it 'rerenders the index view showing the validation errors that occured' do
-
+        it 'redirects to index showing the validation errors that occured' do
+          post :import, addresses: bad_uploaded
+          expect(response).to redirect_to root_url
+          expect(flash[:error]).to be_present
         end
       end
     end
 
     context "without params['addresses']" do
-      it 'rerenders the index view with a notice to select a file to import' do
-
+      it 'redirects to index with a notice to select a file to import' do
+        post :import
+        expect(response).to redirect_to root_url
       end
     end
   end
