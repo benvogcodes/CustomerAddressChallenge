6799665315f4b0c6d9ba153f75d66757237849fd
benvogcodes <benjamin.voglewede@gmail.com>
2016-06-30 15:18:35 -0700

    Add support/factory_girl.rb and associated require statement in spec_helper, add validation tests to model spec.

--- spec/models/customer_shipping_address_spec.rb
+++ spec/models/customer_shipping_address_spec.rb
@@ -1,27 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe CustomerShippingAddress, type: :model do
+  let(:address1){ build(:customer_shipping_address) }
+  let(:address2){ build(:customer_shipping_address, first_name: nil) }
+  let(:address3){ build(:customer_shipping_address, last_name: nil) }
+  let(:address4){ build(:customer_shipping_address, address: nil) }
+  let(:address5){ build(:customer_shipping_address, city: nil) }
+  let(:address6){ build(:customer_shipping_address, state: nil) }
+  let(:address7){ build(:customer_shipping_address, zip_code: nil) }
+
   context "create" do
-    it "is valid with a first_name, last_name, address, city, state and zip_code"
-    it "is invalid without a first_name"
-    it "is invalid without a last_name"
-    it "is invalid without an address"
-    it "is invalid without a city"
-    it "is invalid without a state"
-    it "is invalid without a zip_code"
+    it "is valid with a first_name, last_name, address, city, state and zip_code" do
+      expect(address1.valid?).to eq(true)
+    end
+
+    it "is invalid without a first_name" do
+      expect(address2.valid?).to eq(false)
+    end
+
+    it "is invalid without a last_name" do
+      expect(address3.valid?).to eq(false)
+    end
+
+    it "is invalid without an address" do
+      expect(address4.valid?).to eq(false)
+    end
+
+    it "is invalid without a city" do
+      expect(address5.valid?).to eq(false)
+    end
+
+    it "is invalid without a state" do
+      expect(address6.valid?).to eq(false)
+    end
+
+    it "is invalid without a zip_code" do
+      expect(address7.valid?).to eq(false)
+    end
   end
 
   context "import" do
-    it "creates an object for every non-header row in the import file"
+    pending "creates an object for every non-header row in the import file" do
+
+    end
   end
 
   context "validate_import" do
-    it "returns true if there are no validation warnings"
-    it "returns false if there are any validation warnings"
+    pending "returns true if there are no validation warnings" do
+
+    end
+
+    pending "returns false if there are any validation warnings" do
+
+    end
   end
 
   context "get_validation_warnings" do
-    it "returns an array with validation warnings if any column values are not present"
-    it "returns an empty array if all column values are present"
+    pending "returns an array with validation warnings if any column values are not present" do
+
+    end
+
+    pending "returns an empty array if all column values are present" do
+
+    end
   end
 end
