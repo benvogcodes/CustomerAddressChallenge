865ab6c69a663232254b155023f48cb32be8cca5
benvogcodes <benjamin.voglewede@gmail.com>
2016-06-29 22:22:46 -0700

    Add csv parser to CustomerShippingAddressesController create action

--- app/controllers/customer_shipping_addresses_controller.rb
+++ app/controllers/customer_shipping_addresses_controller.rb
@@ -1,7 +1,24 @@
+require 'csv'
+
 class CustomerShippingAddressesController < ApplicationController
   def index
   end
 
   def create
+    address_data = params['addresses'].tempfile
+
+    CSV.foreach(address_data, :headers => true) do |row|
+      obj_hash = {
+        first_name: row[0],
+        last_name: row[1],
+        address: row[2],
+        city: row[3],
+        state: row[4],
+        zip_code: row[5]
+      }
+      CustomerShippingAddress.create!(obj_hash)
+    end
+
+    redirect_to root_path
   end
 end
