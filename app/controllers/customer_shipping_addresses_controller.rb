aec690c1a535d19a7a62bfeed216b42fe2566f6c
benvogcodes <benjamin.voglewede@gmail.com>
2016-06-30 09:35:48 -0700

    Add error handling for upload, div separation for index view

--- app/controllers/customer_shipping_addresses_controller.rb
+++ app/controllers/customer_shipping_addresses_controller.rb
@@ -4,8 +4,12 @@ class CustomerShippingAddressesController < ApplicationController
   end
 
   def import
-    address_data = params['addresses'].tempfile
-    CustomerShippingAddress.import(address_data)
-    redirect_to root_path
+    if params['addresses']
+      address_data = params['addresses'].tempfile
+      CustomerShippingAddress.import(address_data)
+      redirect_to root_url, notice: "Addresses imported."
+    else
+      redirect_to root_url, notice: "Please select a file to import."
+    end
   end
 end
