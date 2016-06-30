21271fda203c06cda297ed28a51ec7e92ebe56dc
benvogcodes <benjamin.voglewede@gmail.com>
2016-06-30 13:30:11 -0700

    Add CustomerShippingAddress model spec contexts and descriptions

--- spec/models/customer_shipping_address_spec.rb
+++ spec/models/customer_shipping_address_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe CustomerShippingAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "create" do
+    it "is valid with a first_name, last_name, address, city, state and zip_code"
+    it "is invalid without a first_name"
+    it "is invalid without a last_name"
+    it "is invalid without an address"
+    it "is invalid without a city"
+    it "is invalid without a state"
+    it "is invalid without a zip_code"
+  end
+
+  context "import" do
+    it "creates an object for every non-header row in the import file"
+  end
+
+  context "validate_import" do
+    it "returns true if there are no validation warnings"
+    it "returns false if there are any validation warnings"
+  end
+
+  context "get_validation_warnings" do
+    it "returns an array with validation warnings if any column values are not present"
+    it "returns an empty array if all column values are present"
+  end
 end
