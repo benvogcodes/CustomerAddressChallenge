f7fa3b056ed010e90ec94c3120a579116714a7a3
benvogcodes <benjamin.voglewede@gmail.com>
2016-06-30 16:04:21 -0700

    Add CustomerShippingAddressController spec structure.

--- spec/controllers/customer_shipping_addresses_controller_spec.rb
+++ spec/controllers/customer_shipping_addresses_controller_spec.rb
@@ -1,4 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe CustomerShippingAddressesController, type: :controller do
+  describe 'GET #index' do
+    it "populates an array of all CustomerShippingAddresses" do
+
+    end
+
+    it 'renders the index view' do
+
+    end
+  end
+
+  describe 'POST #import' do
+    context "with params['addresses']" do
+      context 'import validation succeeds' do
+        it 'rerenders the index view with a notice that addresses were imported' do
+
+        end
+      end
+
+      context 'import validation fails' do
+        it 'rerenders the index view with a notice that import failed validation' do
+
+        end
+
+        it 'rerenders the index view showing the validation errors that occured' do
+
+        end
+      end
+    end
+
+    context "without params['addresses']" do
+      it 'rerenders the index view with a notice to select a file to import' do
+
+      end
+    end
+  end
 end
