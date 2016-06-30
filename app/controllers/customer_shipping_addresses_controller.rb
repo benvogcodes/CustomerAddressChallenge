2fc0d97722e991397fe6b4882eea2a8717e76a84
benvogcodes <benjamin.voglewede@gmail.com>
2016-06-30 09:16:46 -0700

    Extract import method from CustomerShippingAddressController into CustomerShippingAddress model.

--- app/controllers/customer_shipping_addresses_controller.rb
+++ app/controllers/customer_shipping_addresses_controller.rb
@@ -1,5 +1,3 @@
-require 'csv'
-
 class CustomerShippingAddressesController < ApplicationController
   def index
     @shipping_addresses = CustomerShippingAddress.all
@@ -7,19 +5,7 @@ class CustomerShippingAddressesController < ApplicationController
 
   def import
     address_data = params['addresses'].tempfile
-
-    CSV.foreach(address_data, :headers => true) do |row|
-      obj_hash = {
-        first_name: row[0],
-        last_name: row[1],
-        address: row[2],
-        city: row[3],
-        state: row[4],
-        zip_code: row[5]
-      }
-      CustomerShippingAddress.create!(obj_hash)
-    end
-
+    CustomerShippingAddress.import(address_data)
     redirect_to root_path
   end
 end
--- app/models/customer_shipping_address.rb
+++ app/models/customer_shipping_address.rb
@@ -1,3 +1,19 @@
+require 'csv'
+
 class CustomerShippingAddress < ActiveRecord::Base
   validates :first_name, :last_name, :address, :city, :state, :zip_code, presence: true
+
+  def self.import(file_path)
+    CSV.foreach(file_path, :headers => true) do |row|
+      obj_hash = {
+        first_name: row[0],
+        last_name: row[1],
+        address: row[2],
+        city: row[3],
+        state: row[4],
+        zip_code: row[5]
+      }
+      CustomerShippingAddress.create!(obj_hash)
+    end
+  end
 end
